53360465e839f35d3da9e5123e87957fda0d5a81
mustafizurrahma <mdmustafijur425@gmail.com>
2021-05-02 00:49:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function logIn() {
+  alert(
+    'Now you will go to our facebook page/account. You just follow us to get notified.',
+  )
+}
+
+function signIn() {
+  alert(
+    "If you don't have followed us on Facebook/instagram/Twitter, You have to do this process. Just follow our social sites. If you have already followed, No need to do this again. Click ok. And here we go.",
+  )
+}
